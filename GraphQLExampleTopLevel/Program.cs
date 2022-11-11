77f613b5444b368eaefe3f369ec85fa77382cf95
stescobedo <stescobedo.31@gmail.com>
2022-11-11 07:28:33 -0600

    Added GraphQLServer service

--- GraphQLExampleTopLevel/Program.cs
+++ GraphQLExampleTopLevel/Program.cs
@@ -1,4 +1,5 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGraphQLServer().AddQueryType<LibraryQuery>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
