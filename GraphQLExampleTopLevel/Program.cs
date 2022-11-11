84ef32c35aeebfb5637b4394001db65ea166e2df
stescobedo <stescobedo.31@gmail.com>
2022-11-11 07:31:02 -0600

    Adding base schema for the principal graphql endpoint

--- GraphQLExampleTopLevel/Program.cs
+++ GraphQLExampleTopLevel/Program.cs
@@ -3,6 +3,7 @@ builder.Services.AddGraphQLServer().AddQueryType<LibraryQuery>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseRouting().UseEndpoints(endpoints => endpoints.MapGraphQL());
 
 app.Run();
 
