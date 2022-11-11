b2b51ec101eb0c76db0c847b085fef6819fb056a
stescobedo <stescobedo.31@gmail.com>
2022-11-11 07:36:19 -0600

    Adding LibraryQuery class for retrieve the model and interchange information

--- GraphQLExampleTopLevel/Program.cs
+++ GraphQLExampleTopLevel/Program.cs
@@ -9,3 +9,17 @@ app.Run();
 
 public record Book(string Title, Author Author);
 public record Author(string Name);
+
+public class LibraryQuery
+{
+    readonly List<Book> _books = new()
+    {
+        new Book("I love graphql", new Author("Brandom")),
+        new Book("The pilars of the world", new Author("Example")),
+        new Book("I love the examples of mavericks", new Author("Sergio Triana Escobedo"))
+    };
+
+    public List<Book?> GetBooks() => _books;
+
+    public Book? GetBook(string title) => _books.FirstOrDefault(x => x.Title == title);
+}
